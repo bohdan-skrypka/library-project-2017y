43f2eeffb49fb96127e9861986a1ae720088f49b
bohdan skrypka <skripkabodya@gmail.com>
2024-05-04 01:39:05 +0300

    - initial and the final commit. #for the history. Contains my 2016-2017y. old project code-base I worked on as a e-library to implement.

---
+++ Library/Controllers/AdminController.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Library.Controllers
+{
+    public class AdminController : Controller
+    {
+        // GET: Admin
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        // страница не найдена
+        public ActionResult NotFound()
+        {
+            return View();
+        }
+
+        // таблица пользователей без возможности редактирования
+        public ActionResult BasicTable()
+        {
+            return View();
+        }
+
+        // таблица пользователей для редактирования пользователей
+        public ActionResult ResponsiveTable()
+        {
+            return View();
+        }
+
+        public ActionResult Mail()
+        {
+            return View();
+        }
+        public ActionResult MailResponsive()
+        {
+            return View();
+        }
+        public ActionResult VectorMaps()
+        {
+            return View();
+        }
+        public ActionResult Gallery()
+        {
+            return View();
+        }
+    }
+}
